558a9a51df7dc5be93c91eaa5734a905dfd98881
thepjos <josh.thephasdin@gmail.com>
2023-03-04 11:33:59 -0600

    dark light theme toggle working

--- js/script.js
+++ js/script.js
@@ -4,3 +4,40 @@ const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
 
 // Previously selected theme
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain current theme from interface 
+const getCurrentTheme = () => document.body.classList.contains(darkTheme)?'dark':'light'
+const getCurrentIcon = () => document.body.classList.contains(iconTheme)?'uil-moon':'uil-sun'
+
+// Validate if user has prev chosen topic
+if(selectedTheme){
+    document.body.classList[selectedTheme==='dark'?'add':'remove'](darkTheme)
+    themeButton.classList[selectedIcon==='uil-moon'?'add':'remove'](iconTheme)
+}
+
+// Activate or deactivate the theme
+themeButton.addEventListener('click', () => {
+    // add or remove the light or dark icon
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // save the theme
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+// Menu show Y hidden
+const navMenu = document.getElementById('nav-menu')
+const navToggle =document.getElementById('nav-toggle')
+const navClose =document.getElementById('nav-close')
+
+// Menu show
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+
+
