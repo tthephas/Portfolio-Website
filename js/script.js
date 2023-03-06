cd842cede4698bf55e611647838928c9180c07ea
thepjos <josh.thephasdin@gmail.com>
2023-03-05 18:28:10 -0600

    icons and html for contact section

--- js/script.js
+++ js/script.js
@@ -70,3 +70,26 @@ new Typewriter('#typewriter', {
   loop: true,
   cursor: "|"
 });
+
+
+// Swiper effect for portfolio section
+
+var swiper = new Swiper(".blog-slider", {
+    // cssMode: true,
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel:{
+        invert: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".blog-slider__pagination",
+      clickable: true,
+    },
+    // mousewheel: true,
+    keyboard: true,
+  });
